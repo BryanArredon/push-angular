b40fdbb2c7f77ba6d03483762ed08cea4c9ded48
BryanArredon <byemilio2005@gmail.com>
2026-06-12 17:01:17 -0600

    feat: agregar configuracion de despliegue para vercel

--- src/app/app.spec.ts
+++ src/app/app.spec.ts
@@ -18,6 +18,6 @@ describe('App', () => {
     const fixture = TestBed.createComponent(App);
     fixture.detectChanges();
     const compiled = fixture.nativeElement as HTMLElement;
-    expect(compiled.querySelector('h1')?.textContent).toContain('Hello, push-angular');
+    expect(compiled.querySelector('h1')?.textContent).toContain('Hello, I am Bryan Emilio Arredondo López');
   });
 });
